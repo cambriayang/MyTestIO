6a4eb1eaca968672dc7ad276004d5219b02d52d8
cambriayang <cambriayang@qq.com>
2016-03-27 16:00:06 +0800

    Upload empty files

---
+++ ASDKTestOn4S/ASDKTestOn4S/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ASDKTestOn4S
+//
+//  Created by CambriaYang on 16/3/27.
+//  Copyright © 2016年 CambriaYang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ASDKTestOn4S/ASDKTestOn4S/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ASDKTestOn4S
+//
+//  Created by CambriaYang on 16/3/27.
+//  Copyright © 2016年 CambriaYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
