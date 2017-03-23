11b7ea47bf61e85e1991246dffd85340719bea18
cambriayang <cambriayang@qq.com>
2017-03-23 14:27:25 +0800

    getvc

--- ASDKTestOn4S/ASDKTestOn4S/PureOCViewController.m
+++ ASDKTestOn4S/ASDKTestOn4S/PureOCViewController.m
@@ -20,6 +20,37 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     self.title = NSStringFromClass([self class]);
+    
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(5, 100, 50, 50)];
+    
+    view.backgroundColor = [UIColor redColor];
+    
+    [self.view addSubview:view];
+    
+    UIView *anotherView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 10)];
+    
+    anotherView.backgroundColor = [UIColor blueColor];
+    
+    [view addSubview:anotherView];
+    
+    NSLog(@"%@", [self getViewControllerOfView:anotherView]);
+}
+
+
+/**
+ Get any view's viewcontroller
+
+ @param view
+ @return viewcontroller
+ */
+- (UIViewController *)getViewControllerOfView:(UIView *)view {
+    for (UIView* next = [view superview]; next; next = next.superview) {
+        UIResponder *nextResponder = [next nextResponder];
+        if ([nextResponder isKindOfClass:[UIViewController class]]) {
+            return (UIViewController *)nextResponder;
+        }
+    }
+    return nil;
 }
 
 - (void)didReceiveMemoryWarning {
