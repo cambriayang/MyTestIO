af0c41017a52bc1942822be3ff3788933495912e
cambriayang <cambriayang@qq.com>
2017-04-12 22:33:40 +0800

    add masonry

--- ASDKTestOn4S/ASDKTestOn4S/PureOCViewController.m
+++ ASDKTestOn4S/ASDKTestOn4S/PureOCViewController.m
@@ -8,6 +8,8 @@
 
 #import "PureOCViewController.h"
 
+#import <Masonry.h>
+
 @interface PureOCViewController ()
 
 @end
@@ -41,7 +43,46 @@
     NSLog(@"%@", [[self getViewControllerOfView:anotherView] description]);
 }
 
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    UILabel *l1 = [[UILabel alloc] init];
+    
+    l1.text = @"nihaoooooooooooo";
+    l1.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:l1];
+    
+    UILabel *l2 = [[UILabel alloc] init];
+    
+    l2.text = @"ssssssssssssssssssssssssssssss";
+    l2.backgroundColor = [UIColor yellowColor];
+    
+    [self.view addSubview:l2];
+    
+    UILabel *l3 = [[UILabel alloc] init];
 
+    l3.text = @"tttttttttttttttttttttttttt";
+    l3.backgroundColor = [UIColor purpleColor];
+    
+    [self.view addSubview:l3];
+    
+    [l3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(self.view.mas_right).offset(-15.0);
+        make.top.equalTo(self.view.mas_top).offset(150.0);
+    }];
+    
+    [l1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view.mas_left).offset(15.0);
+        make.top.equalTo(self.view.mas_top).offset(100.0);
+    }];
+    
+    [l2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(l1.mas_right).offset(5.0);
+        make.right.equalTo(l3.mas_left).offset(-10.0);
+        make.top.equalTo(self.view.mas_top).offset(100.0);
+    }];
+}
 /**
  Get the View 's father ViewController as long as the view is in the Viewcontroller's hierarchy
  
