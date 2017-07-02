3cb9e6e17daeb6a45d38bd2ad15dbe27dd01b98a
cambriayang <cambriayang@qq.com>
2017-07-02 13:40:59 +0800

    Supress warning according to xcode9

--- ASDKTestOn4S/ASDKTestOn4S/LFMessage.h
+++ ASDKTestOn4S/ASDKTestOn4S/LFMessage.h
@@ -11,7 +11,7 @@
 NS_ASSUME_NONNULL_BEGIN
 @class LFMessage;
 
-typedef void (^MessageAction) ();
+typedef void (^MessageAction) (void);
 
 typedef NS_ENUM(NSUInteger, LFMessageType) {
     LFMessageTypeDefault = 0,
