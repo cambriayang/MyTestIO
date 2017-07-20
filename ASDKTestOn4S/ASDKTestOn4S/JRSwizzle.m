60eaa0183a59309cf04478d86bfbc8af5665b13b
cambriayang <cambriayang@qq.com>
2017-07-20 20:23:01 +0800

    jrswizzle change

--- ASDKTestOn4S/ASDKTestOn4S/JRSwizzle.m
+++ ASDKTestOn4S/ASDKTestOn4S/JRSwizzle.m
@@ -1,6 +1,6 @@
-// JRSwizzle.m semver:1.0
-//   Copyright (c) 2007-2011 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
-//   Some rights reserved: http://opensource.org/licenses/MIT
+// JRSwizzle.m semver:1.1.0
+//   Copyright (c) 2007-2016 Jonathan 'Wolf' Rentzsch: http://rentzsch.com
+//   Some rights reserved: http://opensource.org/licenses/mit
 //   https://github.com/rentzsch/jrswizzle
 
 #import "JRSwizzle.h"
@@ -40,7 +40,7 @@
 #endif
 		return NO;
 	}
-	
+
 	Method altMethod = class_getInstanceMethod(self, altSel_);
 	if (!altMethod) {
 #if TARGET_OS_IPHONE
@@ -50,7 +50,7 @@
 #endif
 		return NO;
 	}
-	
+
 	class_addMethod(self,
 					origSel_,
 					class_getMethodImplementation(self, origSel_),
@@ -59,13 +59,13 @@
 					altSel_,
 					class_getMethodImplementation(self, altSel_),
 					method_getTypeEncoding(altMethod));
-	
+
 	method_exchangeImplementations(class_getInstanceMethod(self, origSel_), class_getInstanceMethod(self, altSel_));
 	return YES;
 #else
 	//	Scan for non-inherited methods.
 	Method directOriginalMethod = NULL, directAlternateMethod = NULL;
-	
+
 	void *iterator = NULL;
 	struct objc_method_list *mlist = class_nextMethodList(self, &iterator);
 	while (mlist) {
@@ -82,31 +82,39 @@
 		}
 		mlist = class_nextMethodList(self, &iterator);
 	}
-	
+
 	//	If either method is inherited, copy it up to the target class to make it non-inherited.
 	if (!directOriginalMethod || !directAlternateMethod) {
 		Method inheritedOriginalMethod = NULL, inheritedAlternateMethod = NULL;
 		if (!directOriginalMethod) {
 			inheritedOriginalMethod = class_getInstanceMethod(self, origSel_);
 			if (!inheritedOriginalMethod) {
-				SetNSError(error_, @"original method %@ not found for class %@", NSStringFromSelector(origSel_), [self className]);
+#if TARGET_OS_IPHONE
+                SetNSError(error_, @"original method %@ not found for class %@", NSStringFromSelector(origSel_), [self class]);
+#else
+                SetNSError(error_, @"original method %@ not found for class %@", NSStringFromSelector(origSel_), [self className]);
+#endif
 				return NO;
 			}
 		}
 		if (!directAlternateMethod) {
 			inheritedAlternateMethod = class_getInstanceMethod(self, altSel_);
 			if (!inheritedAlternateMethod) {
-				SetNSError(error_, @"alternate method %@ not found for class %@", NSStringFromSelector(altSel_), [self className]);
+#if TARGET_OS_IPHONE
+                SetNSError(error_, @"alternate method %@ not found for class %@", NSStringFromSelector(altSel_), [self class]);
+#else
+                SetNSError(error_, @"alternate method %@ not found for class %@", NSStringFromSelector(altSel_), [self className]);
+#endif
 				return NO;
 			}
 		}
-		
+
 		int hoisted_method_count = !directOriginalMethod && !directAlternateMethod ? 2 : 1;
 		struct objc_method_list *hoisted_method_list = malloc(sizeof(struct objc_method_list) + (sizeof(struct objc_method)*(hoisted_method_count-1)));
         hoisted_method_list->obsolete = NULL;	// soothe valgrind - apparently ObjC runtime accesses this value and it shows as uninitialized in valgrind
 		hoisted_method_list->method_count = hoisted_method_count;
 		Method hoisted_method = hoisted_method_list->method_list;
-		
+
 		if (!directOriginalMethod) {
 			bcopy(inheritedOriginalMethod, hoisted_method, sizeof(struct objc_method));
 			directOriginalMethod = hoisted_method++;
@@ -117,18 +125,42 @@
 		}
 		class_addMethods(self, hoisted_method_list);
 	}
-	
+
 	//	Swizzle.
 	IMP temp = directOriginalMethod->method_imp;
 	directOriginalMethod->method_imp = directAlternateMethod->method_imp;
 	directAlternateMethod->method_imp = temp;
-	
+
 	return YES;
 #endif
 }
 
 + (BOOL)jr_swizzleClassMethod:(SEL)origSel_ withClassMethod:(SEL)altSel_ error:(NSError**)error_ {
 	return [GetClass((id)self) jr_swizzleMethod:origSel_ withMethod:altSel_ error:error_];
+}
+
++ (NSInvocation*)jr_swizzleMethod:(SEL)origSel withBlock:(id)block error:(NSError**)error {
+    IMP blockIMP = imp_implementationWithBlock(block);
+    NSString *blockSelectorString = [NSString stringWithFormat:@"_jr_block_%@_%p", NSStringFromSelector(origSel), block];
+    SEL blockSel = sel_registerName([blockSelectorString cStringUsingEncoding:NSUTF8StringEncoding]);
+    Method origSelMethod = class_getInstanceMethod(self, origSel);
+    const char* origSelMethodArgs = method_getTypeEncoding(origSelMethod);
+    class_addMethod(self, blockSel, blockIMP, origSelMethodArgs);
+
+    NSMethodSignature *origSig = [NSMethodSignature signatureWithObjCTypes:origSelMethodArgs];
+    NSInvocation *origInvocation = [NSInvocation invocationWithMethodSignature:origSig];
+    origInvocation.selector = blockSel;
+
+    [self jr_swizzleMethod:origSel withMethod:blockSel error:nil];
+
+    return origInvocation;
+}
+
++ (NSInvocation*)jr_swizzleClassMethod:(SEL)origSel withBlock:(id)block error:(NSError**)error {
+    NSInvocation *invocation = [GetClass((id)self) jr_swizzleMethod:origSel withBlock:block error:error];
+    invocation.target = self;
+
+    return invocation;
 }
 
 @end
