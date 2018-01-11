a6fc719a1171d3465a2d9ece52d7f0e41e6c5a70
cambriayang <cambriayang@qq.com>
2018-01-11 19:44:08 +0800

    subtle modify

--- MyTestIO/MyTestIO/AppDelegate.m
+++ MyTestIO/MyTestIO/AppDelegate.m
@@ -17,7 +17,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
     int cacheSizeMemory = 4*1024*1024; // 4MB
     int cacheSizeDisk = 32*1024*1024; // 32MB
-    
+
     NSURLCache *sharedCache = [[NSURLCache alloc] initWithMemoryCapacity:cacheSizeMemory diskCapacity:cacheSizeDisk diskPath:nil];
     [NSURLCache setSharedURLCache:sharedCache];
     
