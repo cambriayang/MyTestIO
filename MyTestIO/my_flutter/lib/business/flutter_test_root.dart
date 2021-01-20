e16cafdfbd2987e29e66681167289ad1740ec596
cambriayang <cambriayang@qq.com>
2021-01-20 16:41:55 +0800

    部分flutter demo

--- MyTestIO/my_flutter/lib/business/flutter_test_root.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:core';
-
-import 'package:flutter/cupertino.dart';
-
-class FlutterTestRoot {
-
-}
---
+++ MyTestIO/my_flutter/lib/business/sliver_widgets/custom_sliver_header_usage.dart
@@ -0,0 +1,14 @@
+/*********************************************************************************
+ * Author: Argost Ye
+ * Time: 2021/1/20
+ **********************************************************************************/
+import 'package:flutter/material.dart';
+
+class CustomSliverHeaderDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
