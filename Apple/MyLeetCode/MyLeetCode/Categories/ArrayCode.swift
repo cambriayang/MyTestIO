83bfe7c6e413075b486ccd399f312c61f79e6847
Argost <cambriayang@qq.com>
2022-05-01 23:11:56 +0800

    Add podfile

--- Apple/MyLeetCode/MyLeetCode/Categories/ArrayCode.swift
+++ Apple/MyLeetCode/MyLeetCode/Categories/ArrayCode.swift
@@ -21,123 +21,124 @@ class ArrayCode: LeetCode {
         let _=quickSort(src: &source, left: 0, right: source.count-1)
         print("quickSort after: \(source)")
     }
-    
-    func bubleSort(source: inout [Int]) {
-        print("bubleSort before: \(source)")
-        for i in 0..<source.count {
-            for j in i..<source.count {
-                if source[j]<source[i] {
-                    source.swapAt(i, j)
-                }
+}
+
+
+///Methods
+func bubleSort(source: inout [Int]) {
+    print("bubleSort before: \(source)")
+    for i in 0..<source.count {
+        for j in i..<source.count {
+            if source[j]<source[i] {
+                source.swapAt(i, j)
             }
         }
-        
-        print("bubleSort after: \(source)")
     }
     
-    func quickSort(src: inout [Int], left: Int, right: Int) {
-        if src.count<=1 {
-            return
-        }
-        
-        let left=left
-        let right=right
-        
-        if left<right {
-            //获取pivot的闭包
-            let closure={ (arr: inout [Int], left: Int, right: Int) -> Int in
-                let pivot=left
-                var index=pivot+1
-                
-                for i in index...right {
-                    if arr[i]<arr[pivot] {
-                        arr.swapAt(i, index)
-                        //说明有小于pivot的数字index++
-                        index+=1
-                    }
+    print("bubleSort after: \(source)")
+}
+
+func quickSort(src: inout [Int], left: Int, right: Int) {
+    if src.count<=1 {
+        return
+    }
+    
+    let left=left
+    let right=right
+    
+    if left<right {
+        //获取pivot的闭包
+        let closure={ (arr: inout [Int], left: Int, right: Int) -> Int in
+            let pivot=left
+            var index=pivot+1
+            
+            for i in index...right {
+                if arr[i]<arr[pivot] {
+                    arr.swapAt(i, index)
+                    //说明有小于pivot的数字index++
+                    index+=1
                 }
-                
-                //此处相当于pivot需要更新
-                arr.swapAt(pivot, index-1)
-                return index-1
             }
             
-            let partionIndex=closure(&src, left, right)
-            
-            quickSort(src: &src, left: left, right: partionIndex-1)
-            quickSort(src: &src, left: partionIndex+1, right: right)
-        }
-    }
-
-    //寻找最长上升子序列（不需要连续),10，9，2，5，3，7，101，18，20->2,3,7,18,20
-    func findLongestIncreasingSubsequence(source: [Int]) -> [Int] {
-        let lis = [Int]()
-        
-        return lis
-    }
-
-    func findTheMidNumber(_ data: [Int]) -> [Int] {
-        var res = [Int]()
-        
-        if data.count == 0 {return res}
-        if data.count <= 2 {return res}
-        
-        var rightMin = [Int](repeating: 0, count: data.count)
-        var r_min = data[data.count-1]
-        
-        //从右往左，寻找每个位置及其之后的最小数
-        for i in (0..<data.count).reversed() {
-            if data[i] < r_min {
-                r_min = data[i]
-            }
-            rightMin[i] = r_min
+            //此处相当于pivot需要更新
+            arr.swapAt(pivot, index-1)
+            return index-1
         }
         
-        //从左往右，寻找左边大且比右边小的数
-        var l_max = data[0]
-        for i in 0..<data.count-1 {
-            if data[i] > l_max {
-                l_max = data[i]
-                if data[i] < rightMin[i+1] {
-                    res.append(data[i])
-                }
-            }
-        }
-        print(res)
-        return res
-    }
-
-    func maxProfit(_ prices: [Int]) -> Int {
-        if prices.count < 2 {return 0}
+        let partionIndex=closure(&src, left, right)
         
-        var minPrice = Int.max
-        var maxProfit = 0
-        for i in 0..<prices.count {
-            if prices[i] < minPrice {
-                minPrice = prices[i]
-            } else if prices[i] - minPrice > maxProfit {
-                maxProfit = prices[i]-minPrice
-            }
-        }
-        
-        print(maxProfit)
-        return maxProfit
-    }
-
-    func maxProfitII(_ prices: [Int]) -> Int {
-        if prices.count < 2 {
-            return 0
-        }
-        
-        var maxProfit = 0
-        for i in 1..<prices.count {
-            if prices[i] > prices[i-1] {
-                maxProfit += prices[i]-prices[i-1]
-            }
-        }
-        
-        print(maxProfit)
-        return maxProfit
+        quickSort(src: &src, left: left, right: partionIndex-1)
+        quickSort(src: &src, left: partionIndex+1, right: right)
     }
 }
 
+//寻找最长上升子序列（不需要连续),10，9，2，5，3，7，101，18，20->2,3,7,18,20
+func findLongestIncreasingSubsequence(source: [Int]) -> [Int] {
+    let lis = [Int]()
+    
+    return lis
+}
+
+func findTheMidNumber(_ data: [Int]) -> [Int] {
+    var res = [Int]()
+    
+    if data.count == 0 {return res}
+    if data.count <= 2 {return res}
+    
+    var rightMin = [Int](repeating: 0, count: data.count)
+    var r_min = data[data.count-1]
+    
+    //从右往左，寻找每个位置及其之后的最小数
+    for i in (0..<data.count).reversed() {
+        if data[i] < r_min {
+            r_min = data[i]
+        }
+        rightMin[i] = r_min
+    }
+    
+    //从左往右，寻找左边大且比右边小的数
+    var l_max = data[0]
+    for i in 0..<data.count-1 {
+        if data[i] > l_max {
+            l_max = data[i]
+            if data[i] < rightMin[i+1] {
+                res.append(data[i])
+            }
+        }
+    }
+    print(res)
+    return res
+}
+
+func maxProfit(_ prices: [Int]) -> Int {
+    if prices.count < 2 {return 0}
+    
+    var minPrice = Int.max
+    var maxProfit = 0
+    for i in 0..<prices.count {
+        if prices[i] < minPrice {
+            minPrice = prices[i]
+        } else if prices[i] - minPrice > maxProfit {
+            maxProfit = prices[i]-minPrice
+        }
+    }
+    
+    print(maxProfit)
+    return maxProfit
+}
+
+func maxProfitII(_ prices: [Int]) -> Int {
+    if prices.count < 2 {
+        return 0
+    }
+    
+    var maxProfit = 0
+    for i in 1..<prices.count {
+        if prices[i] > prices[i-1] {
+            maxProfit += prices[i]-prices[i-1]
+        }
+    }
+    
+    print(maxProfit)
+    return maxProfit
+}
--- Apple/MyLeetCode/MyLeetCode/Categories/GreedyCode.swift
+++ Apple/MyLeetCode/MyLeetCode/Categories/GreedyCode.swift
@@ -15,5 +15,14 @@ class GreedyCode : LeetCode {
     }
 }
 
-//LeetCode 455，分糖果
+
+/*LeetCode 455，分糖果，有糖果序列和孩子序列，每个孩子需求一定数量的糖果，如果他得到
+ *满足，则满足他的糖果序列会被消耗，返回最多能满足的孩子数量
+ *如：6个孩子，需求分别是3,10,10,6,16,11。5个糖果序列为9,4,2,7,21
+*/
+func allocationCandies(children: inout [Int], candies: inout [Int]) -> Int {
+    quickSort(src: &children, left: 0, right: children.count-1)
+    quickSort(src: &candies, left: 0, right: candies.count-1)
+    return 0
+}
 
